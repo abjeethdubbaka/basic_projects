fe0e46c17cb2f8471248a4a201d7545f06da52d6
abjeeth <reddy1431289@gmail.com>
2021-01-18 14:34:11 -0500

    small projects

---
+++ Desktop/hello world/Guess_a_num.py
@@ -0,0 +1,24 @@
+import random as r
+
+num = r.randrange(100)
+guess = 5
+
+while guess >=0:
+    your_guess =int(input('Enter your guess: '))
+
+    def check(x):
+        if your_guess == x:
+            print('You Win')
+        elif your_guess > x :
+            print('guess again whith less value')
+        else:
+            print('guess a higher num')
+
+    if guess> 1:
+            check(num)
+    elif guess == 1:
+            check(num)
+            print('this is your last chance ')
+    else:
+            print('you lost')
+            guess -= 1
